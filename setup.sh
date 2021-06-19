eb3493fe9e517c0d4498d619da576584d947dfca
nk-coding <niklask.coding@gmail.com>
2021-06-20 01:26:39 +0200

    finished setup

---
+++ setup.sh
@@ -0,0 +1,16 @@
+git clone https://github.com/ccims/ccims-backend-gql.git
+cd ccims-backend-gql
+cp ../ccims-vsc-setup/config/postgres.json config/postgres.json
+cp ../ccims-vsc-setup/config/api.json config/api.json
+cp ../ccims-vsc-setup/config/markdown.json config/markdown.json
+cp ../ccims-vsc-setup/config/common.json config/common.json
+npm i
+npm run database-file
+cd /tmp
+git clone https://github.com/ccims-vsc/ccims-vsc.git
+cd ccims-vsc
+npm run init-all
+npm i -g vsec
+vsce package
+cd /home/ccims/ccims-vsc
+cp /tmp/ccims-vsc/*.vsix extension.vsix
