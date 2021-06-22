84f0b468670e7e49a9ded41629e7d2f9cdad91cd
nk-coding <niklask.coding@gmail.com>
2021-06-22 21:46:19 +0200

    fixed paths

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@ cd /tmp
 git clone https://github.com/ccims/ccims-backend-gql.git
 cd ccims-backend-gql
 git checkout dev
-cp ../ccims-vsc-setup/config/postgres.json config/postgres.json
-cp ../ccims-vsc-setup/config/api.json config/api.json
-cp ../ccims-vsc-setup/config/markdown.json config/markdown.json
-cp ../ccims-vsc-setup/config/common.json config/common.json
+cp /home/ccims/ccims-vsc-setup/config/postgres.json config/postgres.json
+cp /home/ccims/ccims-vsc-setup/config/api.json config/api.json
+cp /home/ccims/ccims-vsc-setup/config/markdown.json config/markdown.json
+cp /home/ccims/ccims-vsc-setup/config/common.json config/common.json
 npm i
 npm run database-file
 npm i -g typescript
